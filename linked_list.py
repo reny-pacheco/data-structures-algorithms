71162179cce821465e6866c4de21cf2936597c28
reny <renypacheco08.rp@gmail.com>
2021-03-09 18:51:47 +0800

    Modify the Linked List

--- linked_list.py
+++ linked_list.py
@@ -1,128 +1,79 @@
-
-
-class LinkedList:
-    def __init__(self, nodes= None):
-       self.head = None
-       if nodes is not None:
-           node = Node(data=nodes.pop(0))
-           self.head = node
-
-           for elem in nodes: 
-               node.next = Node(data=elem)
-               node = node.next
-
-    def __iter__(self):
-        node = self.head
-
-        while node is not None:
-            yield node
-            node = node.next
-    
-    def __repr__(self):
-        node = self.head
-        nodes = []
-
-        while node is not None:
-            nodes.append(node.data)
-            node = node.next
-        
-        nodes.append("None")
-        return " -> " .join(nodes)
-
-    def add_first(self, node):
-        node.next = self.head
-        self.head = node
-    
-    def add_last(self, node):
-        if self.head is None:
-            self.head = node
-            return
-        for current_node in self:
-            pass
-        current_node.next = node
-    
-    def add_after(self, target_node_data, new_node):
-        if self.head is None:
-            raise Exception("List is empty")
-        
-        for node in self:
-            if node.data == target_node_data:
-                new_node.next = node.next
-                node.next = new_node
-                return
-        raise Exception("Node with data '%s' not found" % target_node_data)
-    
-    def remove_node(self, target_node_data):
-        if self.head is None:
-            raise Exception("List is empty")
-        
-        if self.head.data == target_node_data:
-            self.head = self.head.next
-            return
-
-        previous_node = self.head
-        
-        for node in self:
-            if node.data == target_node_data:
-                previous_node.next = node.next
-                return
-            previous_node = node
-        raise Exception("Node with data '%s' not found" % target_node_data)
-
-    def get_node(self, i):
-        if self.head is None:
-            raise Exception("List is empty")
-        
-        for count, node in enumerate(self, start = 1):
-            if i == node.data:
-                print(count)
-        print('not found')
-
-    def reverse(self):
-        if self.head is None:
-            raise Exception("List is empty")
-        
-        previous_node = None
-        current_node = self.head
-
-        while current_node.next:
-            tmp = current_node.next
-            current_node.next = previous_node
-            previous_node = current_node
-            current_node = tmp
-        
-        current_node.next = previous_node
-        self.head = current_node
-
-           
-        
-
-            
-        
-        
-        
-
-
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
-    def __repr__(self):
-        node = self.data
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
+    #printing the Linked List
+    def traverse(self):
+        temp = self.head
 
-llist = LinkedList(["a", "b", "c", "d"])
+        while(temp):
+            print(temp.data, end=" -> ")
+            temp = temp.next
 
-print(llist)
-llist.reverse()
-print(llist)
+    #adding node at the end
+    def add_e(self, data):
+        new_node = Node(data)
 
-
-
-
-
-
-
-
+        if self.head is None:
+            self.head = new_node
+            return
         
+        node = self.head
+        
+        while(node.next):
+            node = node.next
+        node.next = new_node
+
+    #adding node at the beggining
+    def add_b(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    #deleting a node
+    def del_node(self, data):
+        temp = self.head
+
+        if temp is not None:
+            if temp.data == data:
+                self.head = temp.next
+                temp = None
+                return
+        
+        while temp is not None:
+            if temp.data == data:
+                break
+            prev = temp
+            temp = temp.next
+        
+        if temp is None:
+            return
+        
+        prev.next = temp.next
+        temp = None
+
+
+
+llist = LinkedList()
+llist.head = Node(1)
+second = Node(2)
+third = Node(3)
+
+llist.head.next = second
+second.next = third
+
+llist.traverse()
+
+llist.del_node(3)
+print()
+llist.traverse()
+
+llist.add_e(56)
+print()
+
+llist.traverse()
